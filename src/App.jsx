d7d4ed5a4a763e84a87397403e25b033b91f913d
andresparrab <andres.parrab@gmail.com>
2022-11-19 10:07:45 +0100

    Started an app test

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Homepage from './Homepage/homepage'
+import Navbar from './Navbar/navbar'
+
+function App() {
+  return (
+    <div>
+     <Navbar/> 
+      <h1>App</h1>
+      <Homepage/>
+      </div>
+  );
+}
+
+export default App;
